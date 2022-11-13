849cc184ab2f9262d3046d1fb5824066a0976636
EC2 Default User <ec2-user@ip-172-31-80-84.ec2.internal>
2022-11-13 03:53:31 +0000

    [Create] 投稿機能

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -6,6 +6,10 @@ class Public::PostsController < ApplicationController
   before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   # 投稿機能
+  def new
+    @post = Post.new
+  end
+  
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
@@ -16,9 +20,32 @@ class Public::PostsController < ApplicationController
       render "index"
     end
   end
+  
+  def index
+    @posts = Post.all
+  end
+  
+  def show
+    @post = Post.find(params[:id])
+    @comment = Comment.new
+    @user = @post.user
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
 
   private
   def post_params
     params.require(:post).permit(:body, :golf_spot, :score)
+  end
+  
+  def ensure_correct_user
+    @post = Post.find(params[:id])
+    unless @post.user == current_user
+      redirect_to posts_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
   }
-  
+
   root 'public/homes#top'
-  
+
   # 管理者用
   namespace :admin do
     resources :news, only: [:new, :show, :index, :create]
@@ -22,39 +22,39 @@ Rails.application.routes.draw do
   # scope module: :public publicをpath名から消す
   scope module: :public do
     get 'about' => "homes#about", as: 'about'
-    
+
     resources :posts, only: [:new, :index, :show, :create, :destroy] do
       resource :likes, only: [:create, :destroy]
       resources :comments, only: [:create, :destroy]
     end
-    
+
     resources :users, only: [:index, :show, :edit, :update] do
       resource :relationships, only: [:create, :destroy]
       get 'followings' => 'relationships#followings', as: 'followings'
       get 'followers' => 'relationships#followers', as: 'followers'
     end
-      
+
     get "users/mypage" => "users#show"
     get "users/information/edit" => "users#edit"
     patch "users/information" => "users#update"
-  
+
     get "users/unsubscribe" => "users#unsubscribe"
     patch "users/withdraw" => "users#withdraw"
-      
+
     get 'chat_message/:id' => 'chat_messages#show', as: 'chat_message'
     resources :chat_messages, only: [:create]
-    
+
     resources :chat_rooms, only: [:index]
-    
+
     resources :rankings, only: [:show]
-    
+
     resources :golf_spots, only: [:index, :show]
-    
+
   end
-  
+
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
