d53ab9b4a1c76683ef35db338844f83b9be0263d
EC2 Default User <ec2-user@ip-172-31-80-84.ec2.internal>
2022-11-28 12:20:18 +0000

    [Update] ranking/show

--- app/controllers/public/rankings_controller.rb
+++ app/controllers/public/rankings_controller.rb
@@ -6,7 +6,6 @@ class Public::RankingsController < ApplicationController
     @ranked_posts = @golf_spot.posts.order("score ASC").limit(5)
     @post = current_user.posts.where(golf_spot_id: @golf_spot.id).order(score: :ASC).first
     scores = @golf_spot.posts.order("score ASC").pluck(:score)
-    
     if @post.present? && @post.score
       @rank = scores.index(@post.score)+1
     else @rank = "-"
