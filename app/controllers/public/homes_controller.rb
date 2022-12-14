6b531148efed3333b6f716b1044e9f5fce4112d8
EC2 Default User <ec2-user@ip-172-31-80-84.ec2.internal>
2022-12-14 11:52:47 +0000

    [Fix] home

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -4,15 +4,17 @@ class Public::HomesController < ApplicationController
     @users = User.limit(3).order(created_at: :desc)
     @posts = Post.includes(:user).where(user: { is_deleted: false } ).limit(5).order(created_at: :desc)
     @news = News.limit(3).order(created_at: :desc)
-    @user = current_user
-    unless @user.id == current_user.id
-      chat_room_id = (ChatMember.where(user_id: @user.id).pluck(:chat_room_id) & ChatMember.where(user_id: current_user.id).pluck(:chat_room_id)).first
-      if !!chat_room_id
-        @chat_room = ChatRoom.find(chat_room_id)
-      else
-        @chat_room = ChatRoom.create
-        ChatMember.create(chat_room_id: @chat_room.id, user_id: @user.id)
-        ChatMember.create(chat_room_id: @chat_room.id, user_id: current_user.id)
+    if user_signed_in?
+      @user = current_user
+      unless @user.id == current_user.id
+        chat_room_id = (ChatMember.where(user_id: @user.id).pluck(:chat_room_id) & ChatMember.where(user_id: current_user.id).pluck(:chat_room_id)).first
+        if !!chat_room_id
+          @chat_room = ChatRoom.find(chat_room_id)
+        else
+          @chat_room = ChatRoom.create
+          ChatMember.create(chat_room_id: @chat_room.id, user_id: @user.id)
+          ChatMember.create(chat_room_id: @chat_room.id, user_id: current_user.id)
+        end
       end
     end
   end
